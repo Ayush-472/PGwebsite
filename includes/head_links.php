e28fb7ef2e3726d0494a7a38f7ae90a5afc5e926
Ayush-472 <95129649+Ayush-472@users.noreply.github.com>
2022-11-17 17:27:53 +0530

    Add files via upload

---
+++ includes/head_links.php
@@ -0,0 +1,6 @@
+<link rel="Stylesheet" href="Css/common.css" type="Text/Css">
+<link rel="Stylesheet" href="Css/home.css" type="Text/Css">
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet" />
+<meta charset="utf-8" name="viewport" content= "width=device-width, initial-scale=1.0">
